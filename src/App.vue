5b11924c3891d1066e2730a41e903b92b24db839
wch-android <wangchonghui2219@126.com>
2019-01-08 18:10:26 +0800

    sa

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,12 @@
 </template>
 
 <script>
-
 export default {
   name: 'App'
 }
 </script>
 
 <style>
-
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
